9d5959c0a8c2f7246c820c10f4836520038545ab
amiyzku <amiyzku@gmail.com>
2023-11-28 19:24:25 +0900

    feat: add custom errors

---
+++ src/error.rs
@@ -0,0 +1,17 @@
+#[derive(thiserror::Error, Debug)]
+pub enum AppError {
+    #[error("Websocket connection error: {0}")]
+    WebsocketConnectionError(tokio_tungstenite::tungstenite::Error),
+
+    #[error("Websocket connection closed")]
+    WebsocketConnectionClosed(),
+
+    #[error("Json serialize error: {0}")]
+    JsonSerializeError(serde_json::Error),
+
+    #[error("Json deserialize error: {0}")]
+    JsonDeserializeError(serde_json::Error),
+
+    #[error("Subscribe error: {0}")]
+    SubscribeError(String),
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+pub mod error;
 mod exchanges;
 pub mod response;
