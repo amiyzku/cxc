8894ecc3c7d6fd73e04a0db3cf5753074c3fad80
amiyzku <amiyzku@gmail.com>
2023-11-28 19:21:04 +0900

    feat: add exchange trait

---
+++ src/exchanges/exchange.rs
@@ -0,0 +1,45 @@
+use crate::{
+    error::AppError,
+    response::{Kline, Liquidation, Orderbook, Trade},
+};
+
+pub trait Exchange:
+    OrderbookProvider + TradeProvider + KlineProvider + LiquidationProvider
+{
+}
+
+pub trait OrderbookProvider {
+    type Params;
+    async fn watch_orderbook(
+        &self,
+        params: Self::Params,
+        callback: impl FnMut(Result<AppError, Orderbook>) + Send + 'static,
+    );
+}
+
+pub trait TradeProvider {
+    type Params;
+    async fn watch_trade(
+        &self,
+        params: Self::Params,
+        callback: impl FnMut(Result<AppError, Trade>) + Send + 'static,
+    );
+}
+
+pub trait KlineProvider {
+    type Params;
+    async fn watch_trade(
+        &self,
+        params: Self::Params,
+        callback: impl FnMut(Result<AppError, Kline>) + Send + 'static,
+    );
+}
+
+pub trait LiquidationProvider {
+    type Params;
+    async fn watch_trade(
+        &self,
+        params: Self::Params,
+        callback: impl FnMut(Result<AppError, Liquidation>) + Send + 'static,
+    );
+}
