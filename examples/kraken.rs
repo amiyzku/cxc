7f68da70b799e621e9f846e2c3be43d3ad8db4eb
amiyzku <amiyzku@gmail.com>
2023-12-06 18:41:02 +0900

    chore(Kraken): Add example

--- examples/kraken.rs
+++ examples/kraken.rs
@@ -6,65 +6,74 @@ use cxc::exchanges::{
         request_params::{Depth, Interval},
     },
 };
+use futures_util::future::join_all;
 
 #[tokio::main]
 async fn main() {
     let mut kraken = Kraken::new();
-    // let handle = kraken
-    //     .watch_orderbook(
-    //         OrderbookParams {
-    //             channel: Channel::MainNetPublic,
-    //             depth: Depth::OneHundred,
-    //             symbol: "BTC/USD".to_string(),
-    //         },
-    //         |orderbook| match orderbook {
-    //             Ok(orderbook) => {
-    //                 println!("{:?}", orderbook);
-    //             }
-    //             Err(e) => {
-    //                 println!("{}", e);
-    //             }
-    //         },
-    //     )
-    //     .await
-    //     .unwrap();
+    let mut tasks = vec![];
 
-    // let handle = kraken
-    //     .watch_trade(
-    //         TradeParams {
-    //             channel: Channel::MainNetPublic,
-    //             symbol: "BTC/USD".to_string(),
-    //         },
-    //         |trade| match trade {
-    //             Ok(trade) => {
-    //                 println!("{:?}", trade);
-    //             }
-    //             Err(e) => {
-    //                 println!("{}", e);
-    //             }
-    //         },
-    //     )
-    //     .await
-    //     .unwrap();
+    tasks.push(
+        kraken
+            .watch_orderbook(
+                OrderbookParams {
+                    channel: Channel::MainNetPublic,
+                    depth: Depth::OneHundred,
+                    symbol: "BTC/USD".to_string(),
+                },
+                |orderbook| match orderbook {
+                    Ok(orderbook) => {
+                        println!("{:?}", orderbook);
+                    }
+                    Err(e) => {
+                        println!("{}", e);
+                    }
+                },
+            )
+            .await
+            .expect("Failed to watch orderbook"),
+    );
 
-    let handle = kraken
-        .watch_kline(
-            KlineParams {
-                channel: Channel::MainNetPublic,
-                symbol: "BTC/USD".to_string(),
-                interval: Interval::OneMinute,
-            },
-            |kline| match kline {
-                Ok(kline) => {
-                    println!("{:?}", kline);
-                }
-                Err(e) => {
-                    println!("{}", e);
-                }
-            },
-        )
-        .await
-        .unwrap();
+    tasks.push(
+        kraken
+            .watch_trade(
+                TradeParams {
+                    channel: Channel::MainNetPublic,
+                    symbol: "BTC/USD".to_string(),
+                },
+                |trade| match trade {
+                    Ok(trade) => {
+                        println!("{:?}", trade);
+                    }
+                    Err(e) => {
+                        println!("{}", e);
+                    }
+                },
+            )
+            .await
+            .expect("Failed to watch trade"),
+    );
 
-    let _ = tokio::join!(handle);
+    tasks.push(
+        kraken
+            .watch_kline(
+                KlineParams {
+                    channel: Channel::MainNetPublic,
+                    symbol: "BTC/USD".to_string(),
+                    interval: Interval::OneMinute,
+                },
+                |kline| match kline {
+                    Ok(kline) => {
+                        println!("{:?}", kline);
+                    }
+                    Err(e) => {
+                        println!("{}", e);
+                    }
+                },
+            )
+            .await
+            .expect("Failed to watch kline"),
+    );
+
+    let _ = join_all(tasks).await;
 }
